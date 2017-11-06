850680d1238986d07a78e9dc069e025463e111ca
Marlon Olaya Acevedo <molaya@ias.com.co>
2017-11-06 10:02:44 -0500

    LEDS Funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@angular/http';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -10,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LedProvider } from '../providers/led/led';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -34,7 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LedProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
+import { LedProvider } from './../../providers/led/led';
+import { Led } from './../../model/led.interface';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +8,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  leds: Led[] = [];
+  ledoff = 'ledoff.jpeg';
+  constructor(public navCtrl: NavController, private _ledSrv: LedProvider) {
 
-  constructor(public navCtrl: NavController) {
+    this.leds.push({ title: 'LED UNO', gpio: 11, value: 0, type: 1, color: 'danger', icon: 'ledred.jpeg' });
+    this.leds.push({ title: 'LED DOS', gpio: 7, value: 0, type: 1, color: 'secondary', icon: 'ledgreen.jpeg' });
+    this.leds.push({ title: 'LED TRES', gpio: 12, value: 0, type: 1, color: 'primary', icon: 'ledblue.jpeg' });
 
+    for( let i = 0; i< 3; i++) {
+      this._ledSrv.statusLed(this.leds[i].gpio, this.leds[i].type).subscribe(data => {
+        console.log('probando', data);
+        let json = data.json();
+        this.leds[i].value = json.value;        
+      });
+    }
+  }
+
+  onClick(i) {
+    console.log('Clicked');
+    let value = this.leds[i].value
+    this.leds[i].value = value == 1 ? 0 : 1;
+    this._ledSrv.onOffLed(this.leds[i].title, this.leds[i].gpio, this.leds[i].value, 1).subscribe(data => {
+      console.log('OK CONSUMO', data);
+    }, err => {
+      console.error('No se pudo consumir: ', err);
+    });
   }
 
 }
